de7437cd1d14b87021c28de6c0b2dc135942a5e3
ICEBERG-BUNGA <142441963+ICEBERG-BUNGA@users.noreply.github.com>
2023-08-21 15:24:06 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Calculator {
+public class Main {
     public static void main(String[] args) throws ScannerException {
         Converter converter = new Converter();
         String[] actions = {"+", "-", "/", "*"};
